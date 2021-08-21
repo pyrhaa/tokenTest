1cde5b7e4a81a64eff8d8cdd9461d6a6cb09647c
pyrhaa <sboubtane@protonmail.com>
2021-08-21 17:31:13 +0200

    first commit

---
+++ contracts/Token.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Token {
+    string public name = "Azul";
+    string public symbol = "AZL";
+    uint256 public totalSupply = 10000;
+}
