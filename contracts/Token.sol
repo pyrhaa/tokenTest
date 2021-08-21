c69dc9e3ed5e6c829a5aebac68a381994111630b
pyrhaa <sboubtane@protonmail.com>
2021-08-21 18:28:42 +0200

    func transfer ok

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.0;
 
 contract Token {
+    //variables
     string public name = "Azul";
     string public symbol = "AZL";
     uint256 public totalSupply = 10000;
+    address public owner;
+
+    //mapping
+    mapping(address => uint256) balances;
+
+    //constructor
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
 }
